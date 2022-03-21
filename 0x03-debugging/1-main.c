9edb1235f2373411930759a78cecd465930756f8
Ijeoma Onuaju <ijeomaonuajurx@gmail.com>
2022-03-21 15:14:31 +0100

    1st and 2nd assignment

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - causes an infinite loop
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+printf("infinite loop incoming :c\n");
+i = 0;
+/*
+ *
+ *while (i < 10)
+ *{
+ *Infinite loop - N increase of variables[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Do [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D
+ *putchar(i);
+}
+*/
+orintf ("infinite loop avoided\\o/\n");[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[p[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C!\\o/\n");
+return (0);
+}
+
