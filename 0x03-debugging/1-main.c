ae9152305ede2d439ce84953b0a1bf8a944f53fa
Ijeoma Onuaju <ijeomaonuajurx@gmail.com>
2022-03-21 15:37:05 +0100

    1mainc

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 /**
- *main - causes an infinite loop
+ *main - cause an infinite loop
  *
  *Return: 0
  */
 int main(void)
 {
 int i;
-printf("infinite loop incoming :c\n");
+printf("infinite loop incoming :\n");
 i = 0;
 /*
- *
  *while (i < 10)
  *{
- *Infinite loop - N increase of variables[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Do [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D
+ *Infinite loop - No increase of variables
  *putchar(i);
 }
 */
-orintf ("infinite loop avoided\\o/\n");[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[p[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C!\\o/\n");
+printf("infinite loop avoided! \\o/\n");
 return (0);
 }
-
+
